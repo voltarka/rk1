c1627a6bfaabae43e3a32021cf2900836591290f
voltarka <70635056+voltarka@users.noreply.github.com>
2020-09-30 18:20:03 +0300

    Add files via upload

---
+++ rk1var4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+int main(){
+    double b;
+    double c;
+    std::string str1 = "";
+    std::string str2 = "";
+    std::cout << "Your word: " << std::endl;
+        std::cin >> str1;
+        for (int i = 0; i < str2.size(); i++){
+          switch ()
+
+        }
+        std::cout << str2 << std::endl;
+    return 0;
+}
